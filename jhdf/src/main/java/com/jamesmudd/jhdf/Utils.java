350f0a7497d276d6b74c5a35d1aeb3dc91d69fbc
James Mudd <james.mudd@gmail.com>
2018-11-17 21:09:49 +0000

    Add Utils#readUntilNull method to read names

--- jhdf/src/main/java/com/jamesmudd/jhdf/Utils.java
+++ jhdf/src/main/java/com/jamesmudd/jhdf/Utils.java
@@ -1,6 +1,9 @@
 package com.jamesmudd.jhdf;
 
+import java.nio.ByteBuffer;
+
 public final class Utils {
+	private static final char NULL = '\0';
 	private static final long UNDEFINED_ADDRESS = -1;
 	
 	private Utils() {
@@ -14,4 +17,16 @@ public final class Utils {
 		return "0x" + Long.toHexString(address);
 	}
 
+	
+	public static String readUntilNull(ByteBuffer buffer) {
+		StringBuilder sb = new StringBuilder(buffer.remaining());
+		while (buffer.remaining() > 0)
+		{
+			char c = (char) buffer.get();
+			if (c == NULL)
+				break;
+			sb.append(c);
+		}
+		return sb.toString();
+	}
 }
