ac1a66ee740a30c2c687784b5986478d8e5b2949
James Mudd <james.mudd@gmail.com>
2018-11-18 21:02:22 +0000

    Add UtilsTest

--- jhdf/src/main/java/com/jamesmudd/jhdf/Utils.java
+++ jhdf/src/main/java/com/jamesmudd/jhdf/Utils.java
@@ -3,13 +3,13 @@ package com.jamesmudd.jhdf;
 import java.nio.ByteBuffer;
 
 public final class Utils {
-	private static final char NULL = '\0';
-	private static final long UNDEFINED_ADDRESS = -1;
-	
+	public static final char NULL = '\0';
+	public static final long UNDEFINED_ADDRESS = -1;
+
 	private Utils() {
 		// No instances
 	}
-	
+
 	public static String toHex(long address) {
 		if (address == UNDEFINED_ADDRESS) {
 			return "UNDEFINED";
@@ -17,16 +17,18 @@ public final class Utils {
 		return "0x" + Long.toHexString(address);
 	}
 
-	
 	public static String readUntilNull(ByteBuffer buffer) {
 		StringBuilder sb = new StringBuilder(buffer.remaining());
-		while (buffer.remaining() > 0)
-		{
+		while (buffer.remaining() > 0) {
 			char c = (char) buffer.get();
-			if (c == NULL)
-				break;
+			if (c == NULL) {
+				return sb.toString();
+			}
+			if (!Character.isLetterOrDigit(c)) {
+				throw new IllegalArgumentException("Character '" + c + "' is not a letter or digit");
+			}
 			sb.append(c);
 		}
-		return sb.toString();
+		throw new IllegalArgumentException("End of buffer reached before NULL");
 	}
 }
---
+++ jhdf/src/test/java/com/jamesmudd/jhdf/UtilsTest.java
@@ -0,0 +1,50 @@
+package com.jamesmudd.jhdf;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.nio.ByteBuffer;
+
+import org.junit.Test;
+
+public class UtilsTest {
+
+	@Test
+	public void testToHex() {
+		assertThat(Utils.toHex(88), is(equalTo("0x58")));
+	}
+
+	@Test
+	public void testToHexWithUndefinedAddress() {
+		assertThat(Utils.toHex(Utils.UNDEFINED_ADDRESS), is(equalTo("UNDEFINED")));
+	}
+
+	@Test
+	public void testReadUntilNull() throws Exception {
+		ByteBuffer bb = ByteBuffer.allocate(4);
+		byte[] b = new byte[] { 'H', 'D', 'F', Utils.NULL };
+		bb.put(b);
+		bb.rewind();
+		assertThat(Utils.readUntilNull(bb), is(equalTo("HDF")));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testReadUntilNullThrowsIfNoNullIsFound() throws Exception {
+		ByteBuffer bb = ByteBuffer.allocate(3);
+		byte[] b = new byte[] { 'H', 'D', 'F' };
+		bb.put(b);
+		bb.rewind();
+		Utils.readUntilNull(bb);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testReadUntilNullThrowsIfNonAlphanumericCharacterIsSeen() throws Exception {
+		ByteBuffer bb = ByteBuffer.allocate(3);
+		byte[] b = new byte[] { 'H', 'D', ' ' };
+		bb.put(b);
+		bb.rewind();
+		Utils.readUntilNull(bb);
+	}
+
+}
