8d773d4851be861f9140230a1256f05aaa5ddda7
James Mudd <james.mudd@gmail.com>
2018-12-10 22:37:30 +0000

    Refactor Group to support creation from ObjectHeader address

--- jhdf/src/main/java/com/jamesmudd/jhdf/Group.java
+++ jhdf/src/main/java/com/jamesmudd/jhdf/Group.java
@@ -5,6 +5,8 @@ import java.nio.channels.FileChannel;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import com.jamesmudd.jhdf.object.message.SymbolTableMessage;
+
 public class Group implements Node {
 
 	private final String name;
@@ -12,18 +14,17 @@ public class Group implements Node {
 	private final Group parent;
 	private final Map<String, Node> children;
 
-	private final SymbolTableEntry symbolTableEntry;
 	private final BTreeNode rootbTreeNode;
 	private final LocalHeap rootNameHeap;
 
-	private Group(FileChannel fc, Superblock sb, long steAddress, String name, Group parent) {
+	private Group(FileChannel fc, Superblock sb, long bTreeAddress, long nameHeapAddress, long ojbectHeaderAddress,
+			String name, Group parent) {
 		this.name = name;
-		this.address = steAddress;
+		this.address = ojbectHeaderAddress;
 		this.parent = parent;
 
-		symbolTableEntry = new SymbolTableEntry(fc, steAddress, sb);
-		rootbTreeNode = new BTreeNode(fc, symbolTableEntry.getBTreeAddress(), sb);
-		rootNameHeap = new LocalHeap(fc, symbolTableEntry.getNameHeapAddress(), sb);
+		rootbTreeNode = new BTreeNode(fc, bTreeAddress, sb);
+		rootNameHeap = new LocalHeap(fc, nameHeapAddress, sb);
 
 		final ByteBuffer nameBuffer = rootNameHeap.getDataBuffer();
 
@@ -32,7 +33,7 @@ public class Group implements Node {
 		for (long child : rootbTreeNode.getChildAddresses()) {
 			GroupSymbolTableNode groupSTE = new GroupSymbolTableNode(fc, child, sb);
 			for (SymbolTableEntry e : groupSTE.getSymbolTableEntries()) {
-				String childName = readName(nameBuffer, (int) e.getLinkNameOffset()); // TODO remove this cast
+				String childName = readName(nameBuffer, e.getLinkNameOffset());
 				if (e.getCacheType() == 1) { // Its a group
 					Group group = createGroup(fc, sb, e.getAddress(), childName, this);
 					children.put(childName, group);
@@ -61,11 +62,24 @@ public class Group implements Node {
 	}
 
 	public static Group createGroup(FileChannel fc, Superblock sb, long steAddress, String name, Group parent) {
-		return new Group(fc, sb, steAddress, name, parent);
+		SymbolTableEntry symbolTableEntry = new SymbolTableEntry(fc, steAddress, sb);
+		return new Group(fc, sb, symbolTableEntry.getBTreeAddress(), symbolTableEntry.getNameHeapAddress(),
+				symbolTableEntry.getObjectHeaderAddress(), name, parent);
+	}
+
+	public static Group createGroupFromObjectHeader(FileChannel fc, Superblock sb, long objectHeaderAddress,
+			String name, Group parent) {
+		ObjectHeader oh = ObjectHeader.readObjectHeader(fc, sb, objectHeaderAddress);
+		SymbolTableMessage stm = oh.getMessages().stream().filter(SymbolTableMessage.class::isInstance)
+				.map(SymbolTableMessage.class::cast).findFirst().get();
+
+		return new Group(fc, sb, stm.getbTreeAddress(), stm.getLocalHeapAddress(), objectHeaderAddress, name, parent);
 	}
 
 	protected static Group createRootGroup(FileChannel fc, Superblock sb, long steAddress) {
-		return new RootGroup(fc, sb, steAddress);
+		SymbolTableEntry symbolTableEntry = new SymbolTableEntry(fc, steAddress, sb);
+		return new RootGroup(fc, sb, symbolTableEntry.getBTreeAddress(), symbolTableEntry.getNameHeapAddress(),
+				symbolTableEntry.getObjectHeaderAddress());
 	}
 
 	@Override
@@ -92,9 +106,10 @@ public class Group implements Node {
 		private static final String ROOT_GROUP_NAME = "/";
 		private final long address;
 
-		public RootGroup(FileChannel fc, Superblock sb, long steAddress) {
-			super(fc, sb, steAddress, ROOT_GROUP_NAME, null);
-			this.address = steAddress;
+		public RootGroup(FileChannel fc, Superblock sb, long bTreeAddress, long nameHeapAddress,
+				long ojbectHeaderAddress) {
+			super(fc, sb, bTreeAddress, nameHeapAddress, ojbectHeaderAddress, ROOT_GROUP_NAME, null);
+			this.address = ojbectHeaderAddress;
 		}
 
 		@Override
