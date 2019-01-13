70f08ecee601696e44928e49cfc3379eae1ec3e8
James Mudd <james.mudd@gmail.com>
2019-01-13 18:02:30 +0000

    Add example of reading dataset

--- jhdf/src/main/java/io/jhdf/examples/ReadDataset.java
+++ jhdf/src/main/java/io/jhdf/examples/ReadDataset.java
@@ -1,10 +1,26 @@
 package io.jhdf.examples;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+import io.jhdf.HdfFile;
+import io.jhdf.api.Dataset;
+
 public class ReadDataset {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		long start = System.currentTimeMillis();
 
+		File file = new File(args[0]);
+		System.out.println(file.getName());
+
+		try (HdfFile hdfFile = new HdfFile(file)) {
+			Dataset dataset = hdfFile.getDatasetByPath("/datasets_group/float/float64");
+			System.out.println(Arrays.toString((double[]) dataset.getData()));
+		}
+
+		System.out.println("Took " + (System.currentTimeMillis() - start) + " ms");
 	}
 
 }
