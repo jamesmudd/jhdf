e7c9cecc9f5d1456a849974a0f5a70baed2b3427
James Mudd <james.mudd@gmail.com>
2019-01-28 21:51:47 +0000

    Add tests for medium sized groups - fractal heap but only one direct block

---
+++ jhdf/src/test/resources/io/jhdf/medium_groups.py
@@ -0,0 +1,24 @@
+import h5py
+
+import numpy as np
+
+'''
+The idea of this test if to use a fractal heap but with only one direct block
+'''
+def write_medium_group(f):
+    large_group = f.create_group('large_group')
+    for i in range(20):
+        large_group.create_dataset("data" + str(i), dtype='i4', data=np.array([i]))
+    f.flush()
+    f.close()
+
+if __name__ == '__main__':
+    print('Making medium group test files...')
+    
+    f = h5py.File('test_medium_group_latest.hdf5', 'w', libver='latest')
+    write_medium_group(f)
+    print('created test_medium_file_latest.hdf5')
+        
+    f = h5py.File('test_medium_group_earliest.hdf5', 'w', libver='earliest')
+    write_medium_group(f)
+    print('created test_medium_file_earliest.hdf5')
