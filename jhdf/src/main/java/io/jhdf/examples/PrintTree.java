39ebf5fdd9641769af5da9ea7715b3aa69f7d165
James Mudd <james.mudd@gmail.com>
2019-02-19 20:55:12 +0000

    Cleanup and rename PrintTree example

--- jhdf/src/main/java/io/jhdf/examples/PrintTree.java
+++ jhdf/src/main/java/io/jhdf/examples/PrintTree.java
@@ -1,40 +1,35 @@
 package io.jhdf.examples;
 
+import static java.util.stream.Collectors.joining;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
-import java.util.stream.Collectors;
 
 import io.jhdf.HdfFile;
 import io.jhdf.api.Group;
 import io.jhdf.api.Node;
 
-public class ApiTest {
+public class PrintTree {
 
 	public static void main(String[] args) throws IOException {
-		long start = System.currentTimeMillis();
-
 		File file = new File(args[0]);
 		System.out.println(file.getName());
 
 		try (HdfFile hdfFile = new HdfFile(file)) {
 			recursivePrintGroup(hdfFile, 0);
 		}
-
-		System.out.println("Took " + (System.currentTimeMillis() - start) + " ms");
 	}
 
 	private static void recursivePrintGroup(Group group, int level) {
 		level++;
-		String indent = Collections.nCopies(level, "    ").stream().collect(Collectors.joining(""));
+		String indent = Collections.nCopies(level, "    ").stream().collect(joining());
 		for (Node node : group) {
 			System.out.println(indent + node.getName());
-			System.out.println(indent + node.getAttributes().keySet());
 			if (node instanceof Group) {
 				recursivePrintGroup((Group) node, level);
 			}
 		}
-		level--;
 	}
 
 }
