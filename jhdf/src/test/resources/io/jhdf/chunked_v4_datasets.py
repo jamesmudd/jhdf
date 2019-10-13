ac61118bb35b88bc3e864d7ad64aaa712de9d0cb
James Mudd <james.mudd@gmail.com>
2019-10-13 10:53:01 +0100

    Update test files

--- jhdf/src/test/resources/io/jhdf/chunked_v4_datasets.py
+++ jhdf/src/test/resources/io/jhdf/chunked_v4_datasets.py
@@ -52,15 +52,15 @@ def write_chunked_datasets(f):
     extensible_array_index_group.create_dataset('float32', data=data, dtype='f4', chunks=(2,3), maxshape=(None,3))
     extensible_array_index_group.create_dataset('float64', data=data, dtype='f8', chunks=(2,3), maxshape=(None,3))
     # large data to use secondary blocks smallest chunk size
-    extensible_array_index_group.create_dataset("large_int8", data=large_data, dtype='i1', chunks=(1,1,1), maxshape=(None,5,3))
+    extensible_array_index_group.create_dataset("large_int16", data=large_data, dtype='i2', chunks=(1,1,1), maxshape=(None,5,3))
 
     # B Tree V2 Index - More than one dimension of unlimited extent. Index type 5
-    btree_v2_index_group = f.create_group("btree_v2")
-    btree_v2_index_group.create_dataset("int8", data=data, dtype='i1', chunks=(2,3), maxshape=(None,None))
-    btree_v2_index_group.create_dataset("int16", data=data, dtype='i2', chunks=(2,3), maxshape=(None,None))
-    btree_v2_index_group.create_dataset("int32", data=data, dtype='i4', chunks=(2,3), maxshape=(None,None))
-    btree_v2_index_group.create_dataset('float32', data=data, dtype='f4', chunks=(2,3), maxshape=(None,None))
-    btree_v2_index_group.create_dataset('float64', data=data, dtype='f8', chunks=(2,3), maxshape=(None,None))
+    # btree_v2_index_group = f.create_group("btree_v2")
+    # btree_v2_index_group.create_dataset("int8", data=data, dtype='i1', chunks=(2,3), maxshape=(None,None))
+    # btree_v2_index_group.create_dataset("int16", data=data, dtype='i2', chunks=(2,3), maxshape=(None,None))
+    # btree_v2_index_group.create_dataset("int32", data=data, dtype='i4', chunks=(2,3), maxshape=(None,None))
+    # btree_v2_index_group.create_dataset('float32', data=data, dtype='f4', chunks=(2,3), maxshape=(None,None))
+    # btree_v2_index_group.create_dataset('float64', data=data, dtype='f8', chunks=(2,3), maxshape=(None,None))
 
     f.flush()
     f.close()
