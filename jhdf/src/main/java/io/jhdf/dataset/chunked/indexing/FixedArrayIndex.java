a6789d0a1aa0f1653b81f81375d09e94aa9c885b
James Mudd <james.mudd@gmail.com>
2019-10-13 11:36:47 +0100

    Fixup headers

--- jhdf/src/main/java/io/jhdf/dataset/chunked/indexing/FixedArrayIndex.java
+++ jhdf/src/main/java/io/jhdf/dataset/chunked/indexing/FixedArrayIndex.java
@@ -1,3 +1,12 @@
+/*
+ * This file is part of jHDF. A pure Java library for accessing HDF5 files.
+ *
+ * http://jhdf.io
+ *
+ * Copyright 2019 James Mudd
+ *
+ * MIT License see 'LICENSE' file
+ */
 package io.jhdf.dataset.chunked.indexing;
 
 import io.jhdf.HdfFileChannel;
