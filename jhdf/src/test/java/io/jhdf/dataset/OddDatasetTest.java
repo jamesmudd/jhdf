431366c5a01b5de8ab03c990d97e03a9e06ff9fd
James Mudd <james.mudd@gmail.com>
2019-11-03 13:46:37 +0000

    Add latest odd datasets test file

--- jhdf/src/test/java/io/jhdf/dataset/OddDatasetTest.java
+++ jhdf/src/test/java/io/jhdf/dataset/OddDatasetTest.java
@@ -10,7 +10,6 @@
 package io.jhdf.dataset;
 
 import io.jhdf.HdfFile;
-import io.jhdf.TestUtils;
 import io.jhdf.api.Dataset;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -22,36 +21,48 @@ import java.util.Arrays;
 import java.util.Collection;
 
 import static io.jhdf.TestUtils.flatten;
+import static io.jhdf.TestUtils.loadTestHdfFile;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.DynamicContainer.dynamicContainer;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 class OddDatasetTest {
 
-	private static final String HDF5_TEST_FILE_NAME = "test_odd_datasets_earliest.hdf5";
+	private static final String HDF5_TEST_EARLIEST_FILE_NAME = "test_odd_datasets_earliest.hdf5";
+	private static final String HDF5_TEST_LATEST_FILE_NAME = "test_odd_datasets_latest.hdf5";
 
-	private static HdfFile hdfFile;
+	private static HdfFile earliestHdfFile;
+	private static HdfFile latestHdfFile;
 
 	@BeforeAll
 	static void setup() throws Exception {
-		hdfFile = TestUtils.loadTestHdfFile(HDF5_TEST_FILE_NAME);
+		earliestHdfFile = loadTestHdfFile(HDF5_TEST_EARLIEST_FILE_NAME);
+		latestHdfFile = loadTestHdfFile(HDF5_TEST_LATEST_FILE_NAME);
 	}
 
 	@AfterAll
 	static void tearDown() {
-		hdfFile.close();
+		earliestHdfFile.close();
+		latestHdfFile.close();
 	}
 
 	@TestFactory
-	Collection<DynamicNode> chunkedDatasetReadTests() {
+	Collection<DynamicNode> compressedChunkedDatasetReadTests() {
 		// List of all the datasetPaths
 		return Arrays.asList(
-				dynamicTest("8D_int16", createTest("/8D_int16")),
-				dynamicTest("1D_int16", createTest("/1D_int16")));
+				dynamicContainer(HDF5_TEST_EARLIEST_FILE_NAME, Arrays.asList(
+						dynamicTest("8D_int16", createTest(earliestHdfFile, "/8D_int16")),
+						dynamicTest("1D_int16", createTest(earliestHdfFile, "/1D_int16")))),
+
+				dynamicContainer(HDF5_TEST_LATEST_FILE_NAME, Arrays.asList(
+						dynamicTest("8D_int16", createTest(latestHdfFile, "/8D_int16")),
+						dynamicTest("1D_int16", createTest(latestHdfFile, "/1D_int16"))))
+				);
 	}
 
-	private Executable createTest(String datasetPath) {
+	private Executable createTest(HdfFile hdfFile, String datasetPath) {
 		return () -> {
 			Dataset dataset = hdfFile.getDatasetByPath(datasetPath);
 			Object data = dataset.getData();
