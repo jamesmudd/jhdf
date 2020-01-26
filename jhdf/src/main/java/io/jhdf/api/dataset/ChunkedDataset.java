bcb77dc6c107293c0d01e6f3275e9efe8e24fb2d
James Mudd <james.mudd@gmail.com>
2020-01-26 20:17:13 +0000

    Add headers and Javadoc

--- jhdf/src/main/java/io/jhdf/api/dataset/ChunkedDataset.java
+++ jhdf/src/main/java/io/jhdf/api/dataset/ChunkedDataset.java
@@ -1,13 +1,40 @@
+/*
+ * This file is part of jHDF. A pure Java library for accessing HDF5 files.
+ *
+ * http://jhdf.io
+ *
+ * Copyright 2019 James Mudd
+ *
+ * MIT License see 'LICENSE' file
+ */
 package io.jhdf.api.dataset;
 
 import io.jhdf.api.Dataset;
+import io.jhdf.exceptions.HdfException;
 
 import java.nio.ByteBuffer;
 
+/**
+ * Interface for chunked datasets
+ *
+ * @author James Mudd
+ */
 public interface ChunkedDataset extends Dataset {
 
+    /**
+     * Gets the chunk dimensions of this dataset.
+     *
+     * @return the chunk dimensions of this dataset
+     */
     int[] getChunkDimensions();
 
+    /**
+     * Gets the raw buffer for the specified chunk.
+     *
+     * @param chunkOffset the offset of the required chunk
+     * @return the raw buffer for this chunk
+     * @throws HdfException If the chunk offset is not valid for this dataset
+     */
     ByteBuffer getRawChunkBuffer(int[] chunkOffset);
 
 }
