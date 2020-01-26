e0fd83ff0b559c240ccd7ee9a9c82ae445173510
James Mudd <james.mudd@gmail.com>
2020-01-26 16:16:33 +0000

    Add getRawChunkBuffer tests

--- jhdf/src/test/java/io/jhdf/dataset/ChunkedV4DatasetTest.java
+++ jhdf/src/test/java/io/jhdf/dataset/ChunkedV4DatasetTest.java
@@ -13,22 +13,33 @@ import io.jhdf.HdfFile;
 import io.jhdf.api.Dataset;
 import io.jhdf.api.Group;
 import io.jhdf.api.Node;
+import io.jhdf.api.dataset.ChunkedDataset;
+import io.jhdf.exceptions.HdfException;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DynamicNode;
 import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
 import static io.jhdf.TestUtils.flatten;
+import static io.jhdf.TestUtils.getDimensions;
 import static io.jhdf.TestUtils.loadTestHdfFile;
+import static org.apache.commons.lang3.ArrayUtils.toObject;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.arrayContaining;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isA;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ChunkedV4DatasetTest {
 
@@ -81,5 +92,31 @@ class ChunkedV4DatasetTest {
         });
     }
 
+    @Test
+    void testInvalidChunkOffsetThrows() {
+        Dataset dataset = hdfFile.getDatasetByPath("/fixed_array/int32");
+        assertThat(dataset, isA(ChunkedDataset.class));
+        ChunkedDataset chunkedDataset = (ChunkedDataset) dataset;
+        assertThrows(HdfException.class, () -> chunkedDataset.getRawChunkBuffer(new int[]{2,2}));
+    }
 
+    @Test
+    void testGettingRawChunk() {
+        Dataset dataset = hdfFile.getDatasetByPath("/fixed_array/int32");
+        assertThat(dataset, isA(ChunkedDataset.class));
+        ChunkedDataset chunkedDataset = (ChunkedDataset) dataset;
+        assertThat(toObject(chunkedDataset.getChunkDimensions()), is(arrayContaining(2, 3)));
+
+        ByteBuffer rawChunkBuffer = chunkedDataset.getRawChunkBuffer(new int[]{0, 0});
+        assertThat(rawChunkBuffer.capacity(), is(24));
+
+        rawChunkBuffer.order(ByteOrder.LITTLE_ENDIAN); // set byte order to allow verification of values
+        IntBuffer intBuffer = rawChunkBuffer.asIntBuffer(); // its an int dataset
+        assertThat(intBuffer.capacity(), is(6)); // chunk size is 2x3
+
+        // read the chunk and verify the data
+        int[] chunkData = new int[6];
+        intBuffer.get(chunkData);
+        assertThat(toObject(chunkData), is(arrayContaining(0, 1, 2, 3, 4, 5)));
+    }
 }
