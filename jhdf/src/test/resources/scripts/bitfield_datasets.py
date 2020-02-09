88c2272277a102a9b5f2d09ec5f68f0cd13ed6d3
James Mudd <james.mudd@gmail.com>
2020-02-09 17:56:49 +0000

    Add test files for bitfield datasets

---
+++ jhdf/src/test/resources/scripts/bitfield_datasets.py
@@ -0,0 +1,19 @@
+from tables import *
+import numpy as np
+
+# Open a file in "w"rite mode
+file = open_file("../hdf5/bitfield_datasets.hdf5", mode ="w")
+
+# Create the last table in group2
+data = [bool(i % 2) for i in range(15)]
+file.create_array("/", "bitfield", data)
+file.create_carray("/", "chunked_bitfield", chunkshape=(2,), obj=data)
+filters = Filters(complevel=1, complib='zlib', fletcher32=True)
+file.create_carray("/", "compressed_chunked_bitfield", obj=data, chunkshape=(2,), filters=filters)
+
+data2D = np.array(data).reshape(3,5)
+file.create_carray("/", "compressed_chunked_2d_bitfield", obj=data2D, chunkshape=(2,3), filters=filters)
+
+file.create_array("/", "scalar_bitfield", True)
+
+file.close()
