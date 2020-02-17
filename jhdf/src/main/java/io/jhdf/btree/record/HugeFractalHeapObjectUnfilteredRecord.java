35ebf21eff78314aa4ddc5a4458e67d0a16875b4
alex <alex.andryushkin@corista.com>
2020-02-17 08:47:37 -0500

    add rudimentary support for LargeAttributes

---
+++ jhdf/src/main/java/io/jhdf/btree/record/HugeFractalHeapObjectUnfilteredRecord.java
@@ -0,0 +1,32 @@
+package io.jhdf.btree.record;
+
+import io.jhdf.Utils;
+import io.jhdf.exceptions.HdfException;
+
+import java.nio.ByteBuffer;
+
+public class HugeFractalHeapObjectUnfilteredRecord extends BTreeRecord {
+    private final long hugeObjectAddress;
+    private final long hugeObjectLength;
+    private final long hugeObjectID;
+
+    public HugeFractalHeapObjectUnfilteredRecord(ByteBuffer bb) {
+        if (bb.remaining() < 24) {
+            throw new HdfException(
+                    "Invalid length buffer for HugeFractalHeapObjectUnfilteredRecord. remaining bytes = " + bb.remaining());
+        }
+
+        hugeObjectAddress = Utils.readBytesAsUnsignedLong(bb, 8);
+        hugeObjectLength = Utils.readBytesAsUnsignedLong(bb, 8);
+        hugeObjectID = Utils.readBytesAsUnsignedLong(bb, 8);
+    }
+
+    public long getAddress() {
+        return this.hugeObjectAddress;
+    }
+
+    public long getLength( ) {
+        return this.hugeObjectLength;
+    }
+}
+
