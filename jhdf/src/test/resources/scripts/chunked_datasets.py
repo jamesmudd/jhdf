42469c24fe135105fc48ac157153b52a6fabd98d
James Mudd <james.mudd@gmail.com>
2020-03-14 15:07:50 +0000

    Add test files for half precision (16 bit) floats

--- jhdf/src/test/resources/scripts/chunked_datasets.py
+++ jhdf/src/test/resources/scripts/chunked_datasets.py
@@ -20,6 +20,7 @@ def write_chunked_datasets(f):
     data = np.arange(105).reshape(7,5,3)
 
     float_group = f.create_group('float')
+    float_group.create_dataset('float16', data=data, dtype='f2', chunks=(2,1,3))
     float_group.create_dataset('float32', data=data, dtype='f4', chunks=(2,1,3))
     float_group.create_dataset('float64', data=data, dtype='f8', chunks=(3,4,3))
     
