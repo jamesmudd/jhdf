5ebbec5dfb7edb8bf64f72e8d309cf10ee3ce8f7
James Mudd <james.mudd@gmail.com>
2020-09-25 19:24:02 +0100

    Successfully round trip Superblock <-> bytes

--- jhdf/src/main/java/io/jhdf/BufferBuilder.java
+++ jhdf/src/main/java/io/jhdf/BufferBuilder.java
@@ -6,11 +6,17 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Arrays;
+import java.util.BitSet;
+
+import static java.nio.ByteOrder.LITTLE_ENDIAN;
 
 public class BufferBuilder {
 
 	private final ByteArrayOutputStream byteArrayOutputStream;
-	private final DataOutputStream dataOutputStream;
+	private final DataOutputStream dataOutputStream; // Note always big endian
+	private final ByteOrder byteOrder = LITTLE_ENDIAN;
 
 	public BufferBuilder() {
 		this.byteArrayOutputStream = new ByteArrayOutputStream();
@@ -35,12 +41,50 @@ public class BufferBuilder {
 		}
 	}
 
+	public BufferBuilder writeInt(int i) {
+		try {
+			if(byteOrder == LITTLE_ENDIAN) {
+				i = Integer.reverseBytes(i);
+			}
+			dataOutputStream.writeInt(i);
+			return this;
+		} catch (IOException e) {
+			throw new BufferBuilderException(e);
+		}
+	}
+
+	public BufferBuilder writeLong(long l) {
+		try {
+			if(byteOrder == LITTLE_ENDIAN) {
+				l = Long.reverseBytes(l);
+			}
+			dataOutputStream.writeLong(l);
+			return this;
+		} catch (IOException e) {
+			throw new BufferBuilderException(e);
+		}
+	}
+
 	public ByteBuffer build() {
 		try {
 			ByteBuffer byteBuffer = ByteBuffer.wrap(byteArrayOutputStream.toByteArray());
+			byteBuffer.order(byteOrder);
 			dataOutputStream.close();
 			byteArrayOutputStream.close();
 			return byteBuffer;
+		} catch (IOException e) {
+			throw new BufferBuilderException(e);
+		}
+	}
+
+	public BufferBuilder writeBitSet(BitSet bitSet, int length) {
+		if(bitSet.length() > length) {
+			throw new IllegalArgumentException("BitSet is longer than length provided");
+		}
+		try {
+			final byte[] bytes = Arrays.copyOf(bitSet.toByteArray(), length); // Ensure empty Bitset are not shortened
+			dataOutputStream.write(bytes);
+			return this;
 		} catch (IOException e) {
 			throw new BufferBuilderException(e);
 		}
