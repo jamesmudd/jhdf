0c04d6a316c53e64bf904f5e6992df65fbcab4f2
James Mudd <james.mudd@gmail.com>
2020-10-04 18:55:40 +0100

    Add superblock checksum verification

---
+++ jhdf/src/main/java/io/jhdf/checksum/ChecksumUtils.java
@@ -0,0 +1,37 @@
+/*
+ * This file is part of jHDF. A pure Java library for accessing HDF5 files.
+ *
+ * http://jhdf.io
+ *
+ * Copyright (c) 2020 James Mudd
+ *
+ * MIT License see 'LICENSE' file
+ */
+package io.jhdf.checksum;
+
+import io.jhdf.exceptions.HdfChecksumMismatch;
+
+import java.nio.ByteBuffer;
+
+public class ChecksumUtils {
+
+	public static void validateChecksum(ByteBuffer buffer) {
+		byte[] bytes = new byte[buffer.limit() - 4];
+		buffer.get(bytes);
+		int calculatedChecksum = checksum(bytes);
+		int storedChecksum = buffer.getInt();
+		if(calculatedChecksum != storedChecksum) {
+			throw new HdfChecksumMismatch(storedChecksum, calculatedChecksum);
+		}
+
+	}
+
+	public static int checksum(ByteBuffer buffer) {
+		return JenkinsLookup3HashLittle.hash(buffer.array());
+	}
+
+	public static int checksum(byte[] bytes) {
+		return JenkinsLookup3HashLittle.hash(bytes);
+	}
+
+}
