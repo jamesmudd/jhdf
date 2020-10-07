aad6e8e58cc3a496e5007c5b741ad41d0ad66451
James Mudd <james.mudd@gmail.com>
2020-10-07 22:50:07 +0100

    Add ChecksumUtilsTest

--- jhdf/src/test/java/io/jhdf/JenkinsLookup3HashLittleTest.java
+++ jhdf/src/test/java/io/jhdf/JenkinsLookup3HashLittleTest.java
@@ -26,8 +26,8 @@ class JenkinsLookup3HashLittleTest {
 	static Stream<Arguments> testJenkinsHashLittle() {
 		return Stream.of(
 				//1D
-				Arguments.of("".getBytes(US_ASCII), 0, 0xDEADBEEF),
-				Arguments.of("".getBytes(US_ASCII), 0xDEADBEEF, 0xbd5b7dde),
+				Arguments.of("".getBytes(US_ASCII), 0, 0xdeadbeef),
+				Arguments.of("".getBytes(US_ASCII), 0xdeadbeef, 0xbd5b7dde),
 				Arguments.of("".getBytes(US_ASCII), 0xbd5b7dde, 0x9c093ccd),
 				Arguments.of("Four score and seven years ago".getBytes(US_ASCII), 0, 0x17770551),
 				Arguments.of("Four score and seven years ago".getBytes(US_ASCII), 1, 0xcd628161)
