4a261797207ff799f564e3e26ceeec03321b24e8
James Mudd <james.mudd@gmail.com>
2021-07-02 19:21:38 +0100

    Make writing test read back

--- jhdf/src/test/java/io/jhdf/SimpleWritingTest.java
+++ jhdf/src/test/java/io/jhdf/SimpleWritingTest.java
@@ -2,14 +2,20 @@ package io.jhdf;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class SimpleWritingTest {
 
 	@Test
-	void writeSimpleFile() {
-		WritableHdfFile writableHdfFile = HdfFile.write(Paths.get("test.hdf5"));
+	void writeSimpleFile() throws IOException {
+		Path tempFile = Files.createTempFile(null, ".hdf5");
+		WritableHdfFile writableHdfFile = HdfFile.write(tempFile);
 		writableHdfFile.close();
+
+		// Now read it back
+		new HdfFile(tempFile);
 	}
 }
