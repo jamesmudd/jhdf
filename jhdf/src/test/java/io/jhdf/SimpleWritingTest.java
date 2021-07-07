e12e1d409014e4ca06a95560d597a3e954bd021c
James Mudd <james.mudd@gmail.com>
2021-07-07 20:04:51 +0100

    Write a file that can be opened by HDF5 libs!

--- jhdf/src/test/java/io/jhdf/SimpleWritingTest.java
+++ jhdf/src/test/java/io/jhdf/SimpleWritingTest.java
@@ -10,11 +10,12 @@ import java.nio.file.Paths;
 public class SimpleWritingTest {
 
 	@Test
-	void writeSimpleFile() throws IOException {
+	void writeSimpleFile() throws Exception {
 		Path tempFile = Files.createTempFile(null, ".hdf5");
 		WritableHdfFile writableHdfFile = HdfFile.write(tempFile);
 		writableHdfFile.close();
 
+		Files.copy(tempFile, Paths.get("test.hdf5"));
 		// Now read it back
 		HdfFile hdfFile = new HdfFile(tempFile);
 	}
