2e0a9bda955d0d83873d52da06202fdd03bcdf8d
James Mudd <james.mudd@gmail.com>
2021-11-11 11:24:55 +0000

    Add unit test for DeflatePipelineFilterTest

--- jhdf/src/test/java/io/jhdf/filter/DeflatePipelineFilterTest.java
+++ jhdf/src/test/java/io/jhdf/filter/DeflatePipelineFilterTest.java
@@ -10,11 +10,39 @@
 package io.jhdf.filter;
 
 import io.jhdf.exceptions.HdfFilterException;
+import org.apache.commons.lang3.StringUtils;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.zip.Deflater;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DeflatePipelineFilterTest {
+
+	@Test
+	void decodeWorks() {
+		byte[] input = StringUtils.repeat( "TestString", 50).getBytes(StandardCharsets.UTF_8);
+		byte[] compressedBuffer = new byte[1024];
+
+		Deflater deflater = new Deflater();
+		deflater.setInput(input);
+		deflater.finish(); // This is all the input
+
+		// Do the compression
+		int size = deflater.deflate(compressedBuffer, 0, input.length);
+		byte[] compressed = Arrays.copyOf(compressedBuffer, size);
+
+		DeflatePipelineFilter deflatePipelineFilter = new DeflatePipelineFilter();
+		byte[] decoded = deflatePipelineFilter.decode(compressed, new int[0]);
+
+		assertThat(decoded, is(input));
+	}
 
 	@Test
 	void decodeZeroLengthThrows() {
