1a5a84997ad1092343c779f5226a1ef686875919
James Mudd <james.mudd@gmail.com>
2022-03-22 21:20:30 +0000

    Improve InvalidSliceHdfException message

--- jhdf/src/main/java/io/jhdf/exceptions/InvalidSliceHdfException.java
+++ jhdf/src/main/java/io/jhdf/exceptions/InvalidSliceHdfException.java
@@ -1,14 +1,24 @@
 package io.jhdf.exceptions;
 
+import java.util.Arrays;
+
 public class InvalidSliceHdfException extends HdfException {
-	final long[] offset;
+	final long[] sliceOffset;
 	final int[] sliceDimensions;
 	final int[] datasetDimensions;
 
-	public InvalidSliceHdfException(String message, long[] offset, int[] sliceDimensions, int[] datasetDimensions) {
+	public InvalidSliceHdfException(String message, long[] sliceOffset, int[] sliceDimensions, int[] datasetDimensions) {
 		super(message);
-		this.offset = offset;
+		this.sliceOffset = sliceOffset;
 		this.sliceDimensions = sliceDimensions;
 		this.datasetDimensions = datasetDimensions;
+	}
+
+	@Override
+	public String getMessage() {
+		return super.getMessage() + "sliceOffset=" + Arrays.toString(sliceOffset) +
+			", sliceDimensions=" + Arrays.toString(sliceDimensions) +
+			", datasetDimensions=" + Arrays.toString(datasetDimensions) +
+			'}' ;
 	}
 }
