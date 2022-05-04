e54dd9efca5833b4fe0803ec32663a8c06c55e40
James Mudd <james.mudd@gmail.com>
2022-05-04 15:03:28 +0100

    Fix handling of blocks size

--- jhdf/src/main/java/io/jhdf/filter/BitShuffleFilter.java
+++ jhdf/src/main/java/io/jhdf/filter/BitShuffleFilter.java
@@ -26,12 +26,28 @@ public class BitShuffleFilter implements Filter {
 	}
 
 	@Override public byte[] decode(byte[] encodedData, int[] filterData) {
-		int elementSizeBits = filterData[2] * 8;
-		int blockSize =filterData[3];
+		final int elementSizeBits = filterData[2] * 8;
+		final int blockSize =filterData[3];
+
 		switch (filterData[4]) {
 			case 0: // No compresssion
-				byte[] unshuffled = new byte[encodedData.length];
-				unshuffle(encodedData, elementSizeBits, unshuffled);
+				final int blocks = encodedData.length / blockSize;
+				final byte[] unshuffled = new byte[encodedData.length];
+				for (int i = 0; i < blocks; i++) {
+					byte[] blockData = new byte[blockSize];
+					System.arraycopy(encodedData, i*blockSize, blockData, 0, blockSize);
+					byte[] unshuffledBlock = new byte[blockSize];
+					unshuffle(blockData, elementSizeBits, unshuffledBlock);
+					System.arraycopy(unshuffledBlock, 0, unshuffled, i*blockSize, blockSize);
+				}
+				if(blocks*blockSize < encodedData.length) {
+					int finalBlockSize = encodedData.length - blocks*blockSize;
+					byte[] blockData = new byte[finalBlockSize];
+					System.arraycopy(encodedData, blocks*blockSize, blockData, 0, finalBlockSize);
+					byte[] unshuffledBlock = new byte[finalBlockSize];
+					unshuffle(blockData, elementSizeBits, unshuffledBlock);
+					System.arraycopy(unshuffledBlock, 0, unshuffled, blocks*blockSize, finalBlockSize);
+				}
 				return unshuffled;
 			// https://github.com/kiyo-masui/bitshuffle/blob/master/src/bshuf_h5filter.h#L46
 			case 2: // LZ4
@@ -41,14 +57,14 @@ public class BitShuffleFilter implements Filter {
 				int decompressedBlockSize = byteBuffer.getInt();
 				byte[] decompressed = new byte[Math.toIntExact(totalDecompressedSize)];
 				byte[] decomressedBuffer = new byte[decompressedBlockSize];
-				long blocks = (totalDecompressedSize + decompressedBlockSize - 1) / decompressedBlockSize;
+				long blocks2 = (totalDecompressedSize + decompressedBlockSize - 1) / decompressedBlockSize;
 
 
 				LZ4SafeDecompressor lzz4Decompressor = LZ4Factory.safeInstance().safeDecompressor();
 //				LZ4Factory.safeInstance().safeDecompressor()
 
 				int offset = 0;
-				for (long i = 0; i < blocks; i++) {
+				for (long i = 0; i < blocks2; i++) {
 					int compressedBlockLength = byteBuffer.getInt();
 					byte[] input = new byte[compressedBlockLength];
 					byteBuffer.get(input);
