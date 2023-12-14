b0eb9c118c272be393671ede9887856d6c7e028f
James Mudd <james.mudd@gmail.com>
2023-12-14 21:19:54 +0000

    Work towards scale-offset

--- jhdf/src/main/java/io/jhdf/filter/ScaleOffsetFilter.java
+++ jhdf/src/main/java/io/jhdf/filter/ScaleOffsetFilter.java
@@ -9,6 +9,11 @@
  */
 package io.jhdf.filter;
 
+import io.jhdf.Utils;
+
+import java.util.Arrays;
+import java.util.BitSet;
+
 public class ScaleOffsetFilter implements Filter {
 
 	/**
@@ -33,7 +38,30 @@ public class ScaleOffsetFilter implements Filter {
 
 	@Override
 	public byte[] decode(byte[] encodedData, int[] filterData) {
+		int scaleType = filterData[0];
+		int minBits = filterData[1];
+		int elementCount = filterData[2];
+		int elementBytes = filterData[4];
+		// TODO fill value?
+		// TODO endindness?
 
-		return encodedData;
+		BitSet encodedBits = BitSet.valueOf(encodedData);
+		BitSet decodedBits = new BitSet(elementCount * elementBytes * 8);
+
+		int nBits = 3;
+
+		int encodedBitPos = 0;
+		int decodedBitPos = 0;
+		for (int i = 0; i < elementCount; i++) {
+			for (int j = 0; j < nBits; j++) {
+				decodedBits.set(decodedBitPos++, false);
+			}
+			for (int j = 0; j < elementBytes * 8 - nBits; j++) {
+				decodedBits.set(decodedBitPos++, encodedBits.get(encodedBitPos++));
+			}
+		}
+
+
+		return Arrays.copyOf(decodedBits.toByteArray(), elementCount * elementBytes);
 	}
 }
--- jhdf/src/test/resources/scripts/scale_offset_datasets.py
+++ jhdf/src/test/resources/scripts/scale_offset_datasets.py
@@ -19,11 +19,11 @@ The idea of this test if to write compressed chunked datasets
 def write_chunked_datasets(f):
     data = np.arange(35).reshape(7, 5)
 
-    float_group = f.create_group('float')
-    float_group.create_dataset('float32', data=data, dtype='f4', chunks=(2, 1), scaleoffset=0)
-    float_group.create_dataset('float32_3', data=data, dtype='f4', chunks=(2, 1), scaleoffset=3)
-    float_group.create_dataset('float64', data=data, dtype='f8', chunks=(3, 4), scaleoffset=0)
-    float_group.create_dataset('float64_2', data=data, dtype='f8', chunks=(3, 4), scaleoffset=2)
+    # float_group = f.create_group('float')
+    # float_group.create_dataset('float32', data=data, dtype='f4', chunks=(2, 1), scaleoffset=0)
+    # float_group.create_dataset('float32_3', data=data, dtype='f4', chunks=(2, 1), scaleoffset=3)
+    # float_group.create_dataset('float64', data=data, dtype='f8', chunks=(3, 4), scaleoffset=0)
+    # float_group.create_dataset('float64_2', data=data, dtype='f8', chunks=(3, 4), scaleoffset=2)
 
     int_group = f.create_group('int')
     int_group.create_dataset('int8', data=data, dtype='i1', chunks=(5, 3), scaleoffset=0)
@@ -32,6 +32,16 @@ def write_chunked_datasets(f):
     int_group.create_dataset('int16_3', data=data, dtype='i2', chunks=(1, 1), scaleoffset=3)
     int_group.create_dataset('int32', data=data, dtype='i4', chunks=(1, 3), scaleoffset=0)
     int_group.create_dataset('int32_3', data=data, dtype='i4', chunks=(1, 3), scaleoffset=3)
+
+    data = np.arange(32)
+
+    int_group = f.create_group('int_simple')
+    int_group.create_dataset('int8', data=data, dtype='i1', chunks=(4), scaleoffset=0)
+    int_group.create_dataset('int8_3', data=data, dtype='i1', chunks=(4), scaleoffset=1)
+    int_group.create_dataset('int16', data=data, dtype='i2', chunks=(4), scaleoffset=0)
+    int_group.create_dataset('int16_3', data=data, dtype='i2', chunks=(3), scaleoffset=0)
+    int_group.create_dataset('int32', data=data, dtype='i4', chunks=(4), scaleoffset=0)
+    int_group.create_dataset('int32_3', data=data, dtype='i4', chunks=(5), scaleoffset=0)
 
     f.flush()
     f.close()
