da77fc762d603f174f3a51e638edc81cc2af65bb
James Mudd <james.mudd@gmail.com>
2024-04-20 20:53:50 +0100

    Capture stderr

--- jhdf/src/test/java/io/jhdf/h5dump/H5Dump.java
+++ jhdf/src/test/java/io/jhdf/h5dump/H5Dump.java
@@ -46,6 +46,7 @@ public class H5Dump {
 	public static HDF5FileXml dumpAndParse(Path path) throws IOException, InterruptedException {
 		ProcessBuilder processBuilder = new ProcessBuilder();
 		processBuilder.command("h5dump", "--format=%.1lf", "--xml", path.toAbsolutePath().toString());
+		processBuilder.redirectErrorStream(true); // get stderr as well
 		Process process = processBuilder.start();
 		process.waitFor();
 		String xmlString = IOUtils.toString(process.getInputStream(), StandardCharsets.UTF_8);
