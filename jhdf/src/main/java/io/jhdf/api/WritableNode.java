39fe85f7325027c2453ac9d3efc9bed9d27eca48
James Mudd <james.mudd@gmail.com>
2024-07-21 14:59:56 +0100

    Add javadoc to the WritableNode interface

--- jhdf/src/main/java/io/jhdf/api/WritableNode.java
+++ jhdf/src/main/java/io/jhdf/api/WritableNode.java
@@ -14,8 +14,31 @@ import io.jhdf.storage.HdfFileChannel;
 
 public interface WritableNode extends Node {
 
+	/**
+	 * Writes this node to the file. Not for general use
+	 *
+	 * @param hdfFileChannel The file channel to write this node to
+	 * @param position The position in the file to write this node
+	 * @return bytes written
+	 */
 	long write(HdfFileChannel hdfFileChannel, long position);
 
+	/**
+	 * Adds an attribute to this node
+	 *
+	 * @since v0.8.0
+	 * @param name The attributes name
+	 * @param data The attributes data
+	 * @return The attribute
+	 */
 	Attribute putAttribute(String name, Object data);
+
+	/**
+	 * Removes an attribute from this node
+	 *
+	 * @since v0.8.0
+	 * @param name The attribute to remove
+	 * @return The removed attribute
+	 */
 	Attribute removeAttribute(String name);
 }
