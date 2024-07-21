e39807d71015e414931f657ecac8da2b52f7b97d
James Mudd <james.mudd@gmail.com>
2024-07-21 14:59:41 +0100

    Add writing attributes example

---
+++ jhdf/src/main/java/io/jhdf/examples/WriteAttributes.java
@@ -0,0 +1,35 @@
+/*
+ * This file is part of jHDF. A pure Java library for accessing HDF5 files.
+ *
+ * http://jhdf.io
+ *
+ * Copyright (c) 2024 James Mudd
+ *
+ * MIT License see 'LICENSE' file
+ */
+package io.jhdf.examples;
+
+import io.jhdf.HdfFile;
+import io.jhdf.WritableHdfFile;
+import io.jhdf.api.WritableGroup;
+import io.jhdf.api.WritiableDataset;
+
+import java.nio.file.Paths;
+
+/**
+ * Example application for writing attributes
+ *
+ * @author James Mudd
+ */
+public class WriteAttributes {
+	public static void main(String[] args) {
+		try (WritableHdfFile hdfFile = HdfFile.write(Paths.get("attributes.hdf5"))) {
+			// write scalar attribute on the root group
+			hdfFile.putAttribute("example_group_attribute", 55.543);
+
+			// Write a dataset
+			WritiableDataset dataset = hdfFile.putDataset("ints", new int[]{1, 2, 3, 4});
+			dataset.putAttribute("example_ints_attribute", new int[] {1, 2, 3, 4});
+		}
+	}
+}
