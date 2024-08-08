8281b956fcf47c2450edadfddd945768bc06eecb
James Mudd <james.mudd@gmail.com>
2024-08-08 20:02:42 +0100

    Fix string h5dump parsing

--- jhdf/src/test/java/io/jhdf/h5dump/DataXml.java
+++ jhdf/src/test/java/io/jhdf/h5dump/DataXml.java
@@ -12,11 +12,16 @@ package io.jhdf.h5dump;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.regex.Pattern;
+
 public class DataXml {
+	private static final Pattern PATTERN = Pattern.compile("[\\n\\s\"]{2,}");
 	@JacksonXmlProperty(localName = "DataFromFile")
 	String dataString;
 
 	public String[] getData() {
-		return StringUtils.split(dataString);
+		return PATTERN.splitAsStream(dataString)
+			.filter(StringUtils::isNotBlank)
+			.toArray(String[]::new);
 	}
 }
