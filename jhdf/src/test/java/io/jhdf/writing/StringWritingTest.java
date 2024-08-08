a7da4ada2c25c8ca4421e29bd87fd6769e61c2b9
James Mudd <james.mudd@gmail.com>
2024-08-08 20:35:32 +0100

    Fix headers

--- jhdf/src/test/java/io/jhdf/writing/StringWritingTest.java
+++ jhdf/src/test/java/io/jhdf/writing/StringWritingTest.java
@@ -1,3 +1,13 @@
+/*
+ * This file is part of jHDF. A pure Java library for accessing HDF5 files.
+ *
+ * https://jhdf.io
+ *
+ * Copyright (c) 2024 James Mudd
+ *
+ * MIT License see 'LICENSE' file
+ */
+
 package io.jhdf.writing;
 
 import io.jhdf.HdfFile;
