dd7411b5a6d07aa6d6edf15c4e69939781b447fa
James Mudd <james.mudd@gmail.com>
2024-11-25 18:33:27 +0000

    Add header

--- jhdf/src/test/java/io/jhdf/dataset/chunked/indexing/FixedArrayIndexTest.java
+++ jhdf/src/test/java/io/jhdf/dataset/chunked/indexing/FixedArrayIndexTest.java
@@ -1,3 +1,12 @@
+/*
+ * This file is part of jHDF. A pure Java library for accessing HDF5 files.
+ *
+ * https://jhdf.io
+ *
+ * Copyright (c) 2024 James Mudd
+ *
+ * MIT License see 'LICENSE' file
+ */
 package io.jhdf.dataset.chunked.indexing;
 
 import io.jhdf.HdfFile;
