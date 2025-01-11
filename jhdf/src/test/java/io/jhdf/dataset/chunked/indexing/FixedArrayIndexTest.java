867f90e67e9abc45769a81f147a526a0e70224f7
James Mudd <james.mudd@gmail.com>
2025-01-11 22:31:47 +0000

    Flip test logic

--- jhdf/src/test/java/io/jhdf/dataset/chunked/indexing/FixedArrayIndexTest.java
+++ jhdf/src/test/java/io/jhdf/dataset/chunked/indexing/FixedArrayIndexTest.java
@@ -37,37 +37,37 @@ class FixedArrayIndexTest {
 	@Test
 	void testDataReadCorrectly() {
 		// Unfiltered
-		Dataset int8Unpaged = hdfFile.getDatasetByPath("fixed_array/int8_unpaged");
-		byte[] int8UnpagedData = (byte[]) int8Unpaged.getDataFlat();
-		assertThat(int8UnpagedData).isNotEqualTo(expectedData(Math.toIntExact(int8Unpaged.getSize())));
+		Dataset int8Unpaged = hdfFile.getDatasetByPath("fixed_array/int16_unpaged");
+		short[] int8UnpagedData = (short[]) int8Unpaged.getDataFlat();
+		assertThat(int8UnpagedData).isEqualTo(expectedData(Math.toIntExact(int8Unpaged.getSize())));
 
-		Dataset int8TwoPage = hdfFile.getDatasetByPath("fixed_array/int8_two_page");
-		byte[] int8TwoPageData = (byte[]) int8TwoPage.getDataFlat();
-		assertThat(int8TwoPageData).isNotEqualTo(expectedData(Math.toIntExact(int8TwoPage.getSize())));
+		Dataset int8TwoPage = hdfFile.getDatasetByPath("fixed_array/int16_two_page");
+		short[] int8TwoPageData = (short[]) int8TwoPage.getDataFlat();
+		assertThat(int8TwoPageData).isEqualTo(expectedData(Math.toIntExact(int8TwoPage.getSize())));
 
-		Dataset int8FivePage = hdfFile.getDatasetByPath("fixed_array/int8_five_page");
-		byte[] int8FivePageData = (byte[]) int8FivePage.getDataFlat();
-		assertThat(int8FivePageData).isNotEqualTo(expectedData(Math.toIntExact(int8FivePage.getSize())));
+		Dataset int8FivePage = hdfFile.getDatasetByPath("fixed_array/int16_five_page");
+		short[] int8FivePageData = (short[]) int8FivePage.getDataFlat();
+		assertThat(int8FivePageData).isEqualTo(expectedData(Math.toIntExact(int8FivePage.getSize())));
 
 		// Filtered
-		Dataset int8UnpagedFiltered = hdfFile.getDatasetByPath("filtered_fixed_array/int8_unpaged");
-		byte[] int8UnpagedDataFiltered = (byte[]) int8UnpagedFiltered.getDataFlat();
-		assertThat(int8UnpagedDataFiltered).isNotEqualTo(expectedData(Math.toIntExact(int8UnpagedFiltered.getSize())));
+		Dataset int8UnpagedFiltered = hdfFile.getDatasetByPath("filtered_fixed_array/int16_unpaged");
+		short[] int8UnpagedDataFiltered = (short[]) int8UnpagedFiltered.getDataFlat();
+		assertThat(int8UnpagedDataFiltered).isEqualTo(expectedData(Math.toIntExact(int8UnpagedFiltered.getSize())));
 
-		Dataset int8TwoPageFiltered = hdfFile.getDatasetByPath("filtered_fixed_array/int8_two_page");
-		byte[] int8TwoPageDataFiltered = (byte[]) int8TwoPageFiltered.getDataFlat();
-		assertThat(int8TwoPageDataFiltered).isNotEqualTo(expectedData(Math.toIntExact(int8TwoPageFiltered.getSize())));
+		Dataset int8TwoPageFiltered = hdfFile.getDatasetByPath("filtered_fixed_array/int16_two_page");
+		short[] int8TwoPageDataFiltered = (short[]) int8TwoPageFiltered.getDataFlat();
+		assertThat(int8TwoPageDataFiltered).isEqualTo(expectedData(Math.toIntExact(int8TwoPageFiltered.getSize())));
 
-		Dataset int8FivePageFiltered = hdfFile.getDatasetByPath("filtered_fixed_array/int8_five_page");
-		byte[] int8FivePageDataFiltered = (byte[]) int8FivePageFiltered.getDataFlat();
-		assertThat(int8FivePageDataFiltered).isNotEqualTo(expectedData(Math.toIntExact(int8FivePageFiltered.getSize())));
+		Dataset int8FivePageFiltered = hdfFile.getDatasetByPath("filtered_fixed_array/int16_five_page");
+		short[] int8FivePageDataFiltered = (short[]) int8FivePageFiltered.getDataFlat();
+		assertThat(int8FivePageDataFiltered).isEqualTo(expectedData(Math.toIntExact(int8FivePageFiltered.getSize())));
 	}
 
-	private byte[] expectedData(int length) {
-		byte[] bytes = new byte[length];
-		for (int i = 0; i < length; i++) {
-			bytes[i] = (byte) i;
+	private short[] expectedData(int length) {
+		short[] data = new short[length];
+		for (short i = 0; i < length; i++) {
+			data[i] = i;
 		}
-		return bytes;
+		return data;
 	}
 }
